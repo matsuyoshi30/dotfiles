8d8309ebbf5c6df223e12fba484613fa1a66e20b
matsuyoshi <sfbgwm30@gmail.com>
2020-05-30 15:25:08 +0900

    Add elscreen

--- .emacs.d/inits/01_display.el
+++ .emacs.d/inits/01_display.el
@@ -128,3 +128,30 @@
 (setq pop-up-windows nil)
 (setq split-height-threshold nil)
 (setq split-width-threshold nil)
+
+;; elscreen
+;; http://emacs.rubikitch.com/elscreen/
+(setq elscreen-prefix-key (kbd "C-z"))
+(elscreen-start)
+(setq elscreen-tab-display-kill-screen nil)
+(setq elscreen-tab-display-control nil)
+(setq elscreen-buffer-to-nickname-alist
+      '(("^dired-mode$" .
+         (lambda ()
+           (format "Dired(%s)" dired-directory)))
+        ("^Info-mode$" .
+         (lambda ()
+           (format "Info(%s)" (file-name-nondirectory Info-current-file))))
+        ("^mew-draft-mode$" .
+         (lambda ()
+           (format "Mew(%s)" (buffer-name (current-buffer)))))
+        ("^mew-" . "Mew")
+        ("^irchat-" . "IRChat")
+        ("^liece-" . "Liece")
+        ("^lookup-" . "Lookup")))
+(setq elscreen-mode-to-nickname-alist
+      '(("[Ss]hell" . "shell")
+        ("compilation" . "compile")
+        ("-telnet" . "telnet")
+        ("dict" . "OnlineDict")
+        ("*WL:Message*" . "Wanderlust")))
