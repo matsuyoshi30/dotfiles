b0129b9346ab3a6e2fcb222b085f9ca2d32cf55b
matsuyoshi <sfbgwm30@gmail.com>
2021-08-02 23:50:53 +0900

    Fix: remove unused code

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,44 +8,6 @@
 
 ;;; Code:
 
-;;; Measurement
-
-(defconst my-before-load-init-time (current-time))
-(defconst my-loading-profile-p nil)
-
-(defun my-load-init-time ()
-  "Loading time of user init files including time for `after-init-hook'."
-  (let ((time1 (float-time
-                (time-subtract after-init-time my-before-load-init-time)))
-        (time2 (float-time
-                (time-subtract (current-time) my-before-load-init-time))))
-    (message (concat "Loading init files: %.0f [msec], "
-                     "of which %.f [msec] for `after-init-hook'.")
-             (* 1000 time1) (* 1000 (- time2 time1)))))
-
-(add-hook 'after-init-hook #'my-load-init-time t)
-
-(defvar my-tick-previous-time my-before-load-init-time)
-(defun my-tick-init-time (msg)
-  "Tick boot sequence at loading MSG."
-  (when my-loading-profile-p
-    (let ((ctime (current-time)))
-      (message "---- %5.2f[ms] %s"
-               (* 1000 (float-time
-                        (time-subtract ctime my-tick-previous-time)))
-               msg)
-      (setq my-tick-previous-time ctime))))
-
-(defun my-emacs-init-time ()
-  "Emacs booting time in msec."
-  (message "Emacs booting time: %.0f [msec] = `emacs-init-time'."
-           (* 1000
-              (float-time (time-subtract
-                           after-init-time
-                           before-init-time)))))
-
-(add-hook 'after-init-hook #'my-emacs-init-time) ;; M-x emacs-init-time
-
 ;;; General
 
 (eval-and-compile
