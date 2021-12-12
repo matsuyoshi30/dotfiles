2bc0a55853b7653a8b7f65f9cd6bfbbc91974199
matsuyoshi30 <sfbgwm30@gmail.com>
2021-12-12 09:50:01 +0900

    Add: commentary in early-init.el

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,3 +1,11 @@
+;;; early-init.el --- Early Initialization. -*- lexical-binding: t no-byte-compile: t -*-
+;;; Commentary:
+;;
+;; Emacs 27 introduces early-init.el, which is run before init.el,
+;; before package and UI initialization happens.
+;;
+;;; Code:
+
 (defvar default-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 (setq gc-cons-threshold most-positive-fixnum)
@@ -8,3 +16,4 @@
                   gc-cons-threshold (* 1024 1024 1024))))
 
 (setq byte-compile-warnings '(cl-functions))
+;;; early-init.el ends here
