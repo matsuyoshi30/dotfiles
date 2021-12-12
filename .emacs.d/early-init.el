dee1a311bfd150108101b5033210a2879e31f8ca
matsuyoshi30 <sfbgwm30@gmail.com>
2021-12-12 09:47:42 +0900

    Fix: stop GC when startup

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,1 +1,10 @@
+(defvar default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            "Restore defalut values after startup."
+            (setq file-name-handler-alist default-file-name-handler-alist
+                  gc-cons-threshold (* 1024 1024 1024))))
+
 (setq byte-compile-warnings '(cl-functions))
