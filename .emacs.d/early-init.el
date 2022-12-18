30f409eaf7fa8397d15832af66fb99320c488414
matsuyoshi30 <sfbgwm30@gmail.com>
2022-12-18 14:42:46 +0900

    Fix: early-init

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -9,14 +9,8 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 
-(defvar default-file-name-handler-alist file-name-handler-alist)
-(setq file-name-handler-alist nil)
-(setq gc-cons-threshold most-positive-fixnum)
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            "Restore defalut values after startup."
-            (setq file-name-handler-alist default-file-name-handler-alist
-                  gc-cons-threshold (* 24 1024 1024))))
+(setq inhibit-startup-message t)
+(setq gc-cons-threshold (* 16 1024 1024))
 
 (setq byte-compile-warnings '(cl-functions))
 ;;; early-init.el ends here
