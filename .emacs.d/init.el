9a3745812dddf5f571db7ee2e1c129b84172bed8
matsuyoshi30 <sfbgwm30@gmail.com>
2022-12-18 14:43:09 +0900

    Add: config for booting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,6 +30,50 @@
       (leaf diminish :ensure t)
       (leaf-keywords-init))))
 
+;; (require 'profiler)
+;; (profiler-start 'cpu)
+
+(defconst my/saved-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defconst my/before-load-init-time (current-time))
+
+;;;###autoload
+(defun my/load-init-time ()
+  "Loading time of user init files including time for `after-init-hook'."
+  (let ((time1 (float-time
+                (time-subtract after-init-time my/before-load-init-time)))
+        (time2 (float-time
+                (time-subtract (current-time) my/before-load-init-time))))
+    (message (concat "Loading init files: %.0f [msec], "
+                     "of which %.f [msec] for `after-init-hook'.")
+             (* 1000 time1) (* 1000 (- time2 time1)))))
+(add-hook 'after-init-hook #'my/load-init-time t)
+
+(defvar my/tick-previous-time my/before-load-init-time)
+
+;;;###autoload
+(defun my/tick-init-time (msg)
+  "Tick boot sequence at loading MSG."
+  (when my/loading-profile-p
+    (let ((ctime (current-time)))
+      (message "---- %5.2f[ms] %s"
+               (* 1000 (float-time
+                        (time-subtract ctime my/tick-previous-time)))
+               msg)
+      (setq my/tick-previous-time ctime))))
+
+(defun my/emacs-init-time ()
+  "Emacs booting time in msec."
+  (interactive)
+  (message "Emacs booting time: %.0f [msec] = `emacs-init-time'."
+           (* 1000
+              (float-time (time-subtract
+                           after-init-time
+                           before-init-time)))))
+
+(add-hook 'after-init-hook #'my/emacs-init-time)
+
 (leaf leaf
   :config
   (leaf leaf-convert
@@ -1220,6 +1264,11 @@ by PAD, BEGINNING and END."
   ("C-\\" . nil)
   :config
   (keyboard-translate ?\C-h ?\C-?))
+
+(setq file-name-handler-alist my/saved-file-name-handler-alist)
+
+;; (profiler-report)
+;; (profiler-stop)
 
 (provide 'init)
 ;;; init.el ends here
