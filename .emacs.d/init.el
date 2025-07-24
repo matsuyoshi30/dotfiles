65be3bec4521c25ef67a5150932a771df22bd5c6
Masaya Watanabe <sfbgwm30@gmail.com>
2025-07-24 22:22:09 +0900

    Fix flymake

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -752,42 +752,48 @@
 
 (leaf flymake
   :bind (("M-N" . flymake-goto-next-error)
-         ("M-P" . flymake-goto-prev-error)))
+         ("M-P" . flymake-goto-prev-error))
+  :config
+  (leaf package-lint-flymake :ensure t)
+  (leaf flymake-diagnostic-at-point :ensure t)
+  (leaf popup :ensure t)
+  (leaf posframe :ensure t)
 
-(require 'flymake-diagnostic-at-point)
-(with-eval-after-load 'flymake
-  (add-hook 'flymake-mode-hook #'flymake-diagnostic-at-point-mode)
-  (add-hook 'emacs-lisp-mode-hook #'package-lint-flymake-setup)
-  (set-face-attribute 'popup-tip-face nil
-		      :background "dark slate gray" :foreground "white" :underline nil))
-(remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
+  (with-eval-after-load 'flymake
+    (with-eval-after-load 'flymake-diagnostic-at-point
+      (add-hook 'flymake-mode-hook #'flymake-diagnostic-at-point-mode))
+    (add-hook 'emacs-lisp-mode-hook #'package-lint-flymake-setup)
+    (with-eval-after-load 'popup
+      (set-face-attribute 'popup-tip-face nil
+		                      :background "dark slate gray"
+                          :foreground "white"
+                          :underline nil)))
+  (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
 
-;; flymake-posframe
-(defvar flymake-posframe-hide-posframe-hooks
-  '(pre-command-hook post-command-hook focus-out-hook)
-  "The hooks which should trigger automatic removal of the posframe.")
-
-(defun flymake-posframe-hide-posframe ()
-  "Hide messages currently being shown if any."
-  (posframe-hide " *flymake-posframe-buffer*")
-  (dolist (hook flymake-posframe-hide-posframe-hooks)
-    (remove-hook hook #'flymake-posframe-hide-posframe t)))
-
-(defun my/flymake-diagnostic-at-point-display-popup (text)
-  "Display the flymake diagnostic TEXT inside a posframe."
-  (posframe-show " *flymake-posframe-buffer*"
-		 :string (concat flymake-diagnostic-at-point-error-prefix
-				 (flymake--diag-text
-				  (get-char-property (point) 'flymake-diagnostic)))
-		 :position (point)
-		 :foreground-color "cyan"
-		 :internal-border-width 2
-		 :internal-border-color "red"
-		 :poshandler 'posframe-poshandler-window-bottom-left-corner)
-  (dolist (hook flymake-posframe-hide-posframe-hooks)
-    (add-hook hook #'flymake-posframe-hide-posframe nil t)))
-
-(advice-add 'flymake-diagnostic-at-point-display-popup :override 'my/flymake-diagnostic-at-point-display-popup)
+  ;; flymake-posframe
+   (with-eval-after-load 'flymake-diagnostic-at-point
+     (defvar flymake-posframe-hide-posframe-hooks
+       '(pre-command-hook post-command-hook focus-out-hook)
+       "The hooks which should trigger automatic removal of the posframe.")
+     (defun flymake-posframe-hide-posframe ()
+       "Hide messages currently being shown if any."
+       (posframe-hide " *flymake-posframe-buffer*")
+       (dolist (hook flymake-posframe-hide-posframe-hooks)
+         (remove-hook hook #'flymake-posframe-hide-posframe t)))
+     (defun my/flymake-diagnostic-at-point-display-popup (text)
+       "Display the flymake diagnostic TEXT inside a posframe."
+       (posframe-show " *flymake-posframe-buffer*"
+		                  :string (concat flymake-diagnostic-at-point-error-prefix
+				                              (flymake--diag-text
+				                               (get-char-property (point) 'flymake-diagnostic)))
+		                  :position (point)
+		                  :foreground-color "cyan"
+		                  :internal-border-width 2
+		                  :internal-border-color "red"
+		                  :poshandler 'posframe-poshandler-window-bottom-left-corner)
+       (dolist (hook flymake-posframe-hide-posframe-hooks)
+         (add-hook hook #'flymake-posframe-hide-posframe nil t)))
+     (advice-add 'flymake-diagnostic-at-point-display-popup :override 'my/flymake-diagnostic-at-point-display-popup)))
 
 ;;; Snippet
 
